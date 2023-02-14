0cc3510fca6e9981d9fb2b468926ef39f79be128
icedfool <50580723+icedfool@users.noreply.github.com>
2023-02-14 22:01:53 +0800

    create interface and chest
    just create chest and interface header file, haven't do the implement yet

---
+++ Source/ActionRougelike/Private/GreyDemoGameplayInterface.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GreyDemoGameplayInterface.h"
+
+// Add default functionality here for any IGreyDemoGameplayInterface functions that are not pure virtual.
